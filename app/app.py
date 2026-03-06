033693b361c7ab7561c9394a10fa6842badeddf9
pranavsoni21 <psoniradhe177@gmail.com>
2026-03-06 09:28:26 +0530

    Initial commit with adding code on app/app.py and requirements.txt

---
+++ app/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+users = []
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "DevOps Portfolio API",
+        "status": "running"
+    })
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "healthy"
+    })
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    return jsonify(users)
+
+@app.route("/users", methods=["POST"])
+def create_user():
+    data = request.json
+
+    user = {
+        "id": len(users) + 1,
+        "name": data.get("name"),
+        "email": data.get("email")
+    }
+
+    users.append(user)
+
+    return jsonify(user), 201
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
